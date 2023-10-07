cbb2057fa694dcd81a4ffaba2e48f1997812ea27
DuDeXsUraj <surajgusain2004@gmail.com>
2023-10-07 22:08:03 +0530

    First Commit

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -9,14 +9,15 @@ import "./MyToken.sol";
 contract MyNFT is ERC721, Ownable {
     IERC20 public erc20Token;
     uint256 public tokenIdCounter;
+    uint256 public constant mintingPrice = 100 * 10**18; // Set the fixed minting price to 100 tokens
 
     constructor(address _erc20TokenAddress) ERC721("MyNFT", "MNFT") {
         erc20Token = IERC20(_erc20TokenAddress);
         tokenIdCounter = 0;
     }
 
-    function mintNFT(uint256 price) external {
-        require(erc20Token.transferFrom(msg.sender, address(this), price), "Transfer failed");
+    function mintNFT() external {
+        require(erc20Token.transferFrom(msg.sender, address(this), mintingPrice), "Transfer failed");
         _safeMint(msg.sender, tokenIdCounter);
         tokenIdCounter++;
     }
